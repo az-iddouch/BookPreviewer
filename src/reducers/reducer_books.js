88aace40eb3e3f572aa674218e002b32e8bf9652
abdelaziz <abdelaziz.iddouch@gmail.com>
2018-09-25 02:01:37 +0100

    first commit and created my first redux reducer 'booksReducer', also created my first container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Javascript The Good Parts" },
+    { title: "The Power Of Now" },
+    { title: "The willpower Instinct" },
+    { title: "The Dark Tower" }
+  ];
+}
