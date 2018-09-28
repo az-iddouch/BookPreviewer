17ba66b283fd5aefdca56fcc313ac5c871bbcf0a
abdelaziz <abdelaziz.iddouch@gmail.com>
2018-09-28 22:23:41 +0100

    created the containers, action creators and my reducers

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
-export default class BookList extends React.Component {
+class BookList extends React.Component {
   renderList() {
     return this.props.books.map(book => {
-      return <li className="list-group-item" key={book.title} >{ book.title }</li>
-    })
+      return (
+        <li
+          className="list-group-item"
+          onClick={() => this.props.selectBook(book)}
+          key={book.title}
+        >
+          {book.title}
+        </li>
+      );
+    });
   }
 
   render() {
-    return (
-      <ul className="list-group col-sm-4">
-        { this.renderList() }
-      </ul>
-    )
+    return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
   }
 }
+
+// this function is the glue between React and Redux
+function mapStateToProps(state) {
+  // what ever is returned is gonna show up as props inside of BookList
+  return {
+    books: state.books
+  };
+}
+
+// Anything returned from this function will end up as props on the bookList container
+function mapDispatchToProps(dispatch) {
+  // when ever selectBook is called, the result should be passed to
+  // all of our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+// Promote bookList from a component to a container - it needs to know about this new dispatch method, selectBook. Make it available as props
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BookList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
-import { combineReducers } from "redux";
-import BooksReducer from "./reducer_books";
+import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
+// adds this 'books' property to our state,
+// and assign it the value of our reducer 'BookReducer'
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
